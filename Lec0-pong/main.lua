4a1298da0dfe99712b9bd5c1980c6f4b299065b6
King <24613526+nil-omen@users.noreply.github.com>
2026-04-23 17:05:03 +0200

    Refactor game state management and improve ball movement logic

--- Lec0-pong/main.lua
+++ Lec0-pong/main.lua
@@ -4,45 +4,105 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 push = require 'push'
 
 function love .load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
-
+    
+    math.randomseed(os.time())
+    
     largeFont = love.graphics.newFont('font.ttf', 32)
     smallFont = love.graphics.newFont('font.ttf', 8)
 
-    player1Score = 0
-    player2Score = 0
-
+    love.graphics.setFont(smallFont)
+    
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         resizable = false,
         vsync = true,
         fullscreen = false
     })
-    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
+        push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 10
+    player2Y = VIRTUAL_HEIGHT - 30
+
+    ballX = VIRTUAL_WIDTH / 2 - 2
+    ballY = VIRTUAL_HEIGHT / 2 - 2
+
+    ballDX = math.random(2) == 1 and 100 or -100
+    ballDY = math.random(-50, 50)
+
+    gameState = 'start'
 end
 
 function love.keypressed(key)
     if key == 'escape' then 
         love.event.quit()
+
+    elseif key == 'return' or key == 'space' then 
+        if gameState == 'start' then
+            gameState = 'play'
+        else
+            gameState = 'start'
+
+            ballX = VIRTUAL_WIDTH / 2 - 2
+            ballY = VIRTUAL_HEIGHT / 2 - 2
+
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50, 50) * 1.5
+        end
+    end
+end
+
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('s') then
+        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('down') then
+        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+    end
+
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
     end
 end
 
 function love.draw()
     push.start()
+
     love.graphics.clear(40/255, 45/255, 52/255, 1)
-    love.graphics.setFont(largeFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 2 - 100)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 2 - 100)
+
+    love.graphics.setFont(smallFont)
+
+    -- love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 2 - 100)
+    -- love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 2 - 100)
+
+    if gameState == 'start' then
+        love.graphics.printf('Hello Start State!', 0, 10, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.printf('Hello Play State!', 0, 10, VIRTUAL_WIDTH, 'center')
+    end
 
     -- paddle 1
-    love.graphics.rectangle('fill', 10, 10, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     -- paddle 2
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2Y, 5, 20)
 
     -- ball
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
     push.finish()
 end

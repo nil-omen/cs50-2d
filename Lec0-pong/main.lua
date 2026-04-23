3c23723216a10aaa0fb05e38658dfd423be90adf
King <24613526+nil-omen@users.noreply.github.com>
2026-04-23 14:56:55 +0200

    Add initial implementation of Pong game using LÖVE 2D
    - Created main.lua to handle game logic, rendering, and input.
    - Implemented basic game window setup with push for resolution handling.
    - Added score display for two players and basic paddle and ball graphics.
    - Introduced push.lua for managing canvas and scaling.
    - Added README.md with course information and links to resources.

---
+++ Lec0-pong/main.lua
@@ -0,0 +1,48 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
+function love .load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    largeFont = love.graphics.newFont('font.ttf', 32)
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    player1Score = 0
+    player2Score = 0
+
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        resizable = false,
+        vsync = true,
+        fullscreen = false
+    })
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
+end
+
+function love.keypressed(key)
+    if key == 'escape' then 
+        love.event.quit()
+    end
+end
+
+function love.draw()
+    push.start()
+    love.graphics.clear(40/255, 45/255, 52/255, 1)
+    love.graphics.setFont(largeFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 2 - 100)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 2 - 100)
+
+    -- paddle 1
+    love.graphics.rectangle('fill', 10, 10, 5, 20)
+
+    -- paddle 2
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5, 20)
+
+    -- ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    push.finish()
+end
---
+++ README.md
@@ -0,0 +1,5 @@
+# CS50’s Introduction to 2D Game Development
+
+For source code, notes or slides see the [Website](https://cs50.harvard.edu/games/)
+
+This course fouceses on development of two-dimensional (2D) interactive games using the [Lua](https://www.lua.org/) programming language and [LÖVE 2D](https://love2d.org/) framework.
